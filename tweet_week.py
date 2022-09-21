de2c5e5ef94c345ebf0c58431d22512da897667e
tatsuya0926 <myuthree1478@gmail.com>
2022-09-21 18:36:45 +0900

    modified logger

--- tweet_week.py
+++ tweet_week.py
@@ -2,12 +2,18 @@
 
 import tweepy
 import datetime
+import logging
 import twitter_util
 
 TWEET_FILE_NAME = "week_tweets.txt"
 SPLITTER = "[--split--]"
 
+logging.basicConfig(filename='exec-error.log', level=logging.ERROR, encoding='utf-8')
+
 def post(text):
+    """
+    Twitterに投稿
+    """
     auth = tweepy.OAuthHandler(twitter_util.CONSUMER_KEY, twitter_util.CONSUMER_SECRET)
     auth.set_access_token(twitter_util.ACCESS_TOKEN, twitter_util.ACCESS_SECRET)
     api = tweepy.API(auth)
@@ -15,22 +21,16 @@ def post(text):
     api.update_status(status=text)
 
 def main():
-    with open(TWEET_FILE_NAME, "r", encoding="utf_8") as f: # tweet内容が含まれたtextファイル読み込み
-        file_content = f.read()
-        #print(file_content)
-
-        contents = file_content.split(SPLITTER) # SPLITTERでファイルの中身を区切って配列にする
-        #print(contents)
-
-        week_num = datetime.date.today().weekday() # 曜日番号を取得（0は月曜日）
-        #print(week_num)
-
-        tweet_text = contents[week_num].strip() # tweetファイルの中身から曜日番号に対応する内容を取得
-        #print(tweet_text)
-
-        post(tweet_text) # 投稿
-        # t = twitter_util.TwitterUtil()
-        # t.post(tweet_text) # 投稿
+    try:
+        with open(TWEET_FILE_NAME, "r", encoding="utf_8") as f:
+            file_content = f.read()
+            contents = file_content.split(SPLITTER) # SPLITTERでファイルの中身を区切って配列にする
+            week_num = datetime.date.today().weekday() # 曜日番号を取得（0は月曜日）
+            tweet_text = contents[week_num].strip() # tweetファイルの中身から曜日番号に対応する内容を取得
+            post(tweet_text)
+    except Exception as e:
+        logging.error(e)
+        logging.error("予期せぬエラーが発生しました")
 
 if __name__ == '__main__':
     main()
--- twitter_autoFavorite.py
+++ twitter_autoFavorite.py
@@ -1,24 +1,31 @@
 import tweepy
+import logging
 import twitter_util
 
+logging.basicConfig(filename='exec-error.log', level=logging.ERROR, encoding='utf-8')
+
 def main():
-  
-    auth=tweepy.OAuthHandler(twitter_util.CONSUMER_KEY,twitter_util.CONSUMER_SECRET)
-    auth.set_access_token(twitter_util.ACCESS_TOKEN,twitter_util.ACCESS_SECRET)
-    api=tweepy.API(auth)
+    try:
+        auth=tweepy.OAuthHandler(twitter_util.CONSUMER_KEY,twitter_util.CONSUMER_SECRET)
+        auth.set_access_token(twitter_util.ACCESS_TOKEN,twitter_util.ACCESS_SECRET)
+        api=tweepy.API(auth)
 
+        count=20
+        q_list=["#駆け出しエンジニアと繋がりたい","#AIエンジニア","#機械学習","progate"]
+        for q in q_list:
+            logging.info(f"Now:QUERY-->>{q}")
+            search_results=api.search(q=q,count=count) #ツイートのデータであるstatusオブジェクトを取得
+            for status in search_results:
+                try:
+                    api.create_favorite(status.id) #ファボ
+                except:
+                    pass
 
-    q_list=["#駆け出しエンジニアと繋がりたい","#プログラミング","#プログラミング初学者","プログラミング独学","#Progate"]
-    count=50
-    for q in q_list:
-        print("Now:QUERY-->>{}".format(q))
-        search_results=api.search(q=q,count=count) #ツイートのデータであるstatusオブジェクトを取得
-        for status in search_results:
-            tweet_id=status.id #ツイートidにアクセス
-            try:
-                api.create_favorite(tweet_id) #ファボ
-            except:
-                pass
+    except Exception as e:
+        logging.error("予期せぬエラーが発生しました")
+        logging.error(e)
+    finally:
+        logging.info("処理終了")
 
 if __name__ == '__main__':
     main()
